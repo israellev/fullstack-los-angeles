459500b69a677e4e52f9934904ac0b16476e2124
Sahar160 <saharbe1992@gmail.com>
2023-05-09 13:31:54 +0300

    some message

---
+++ javascript/02 - TS, Operators, Conditions, Math/class-puzzle/sahar/index.ts
@@ -0,0 +1,20 @@
+let IQ = Number(prompt("what is your IQ level (put a number between 1 to 20)"))
+let gender = prompt("Are you a man or a woman?")
+if (gender === "man") {
+    } else if
+    (IQ < 10 ) {
+        alert("את אהבל'ה!")
+        }
+    else {
+        alert("את גאון חביבתי!")  
+      } 
+
+    if (gender === "woman") {
+    } else if
+    (IQ < 10 ) {
+        alert("אתה אהבל!")
+        }
+    else {
+        alert("אתה גאון חביבי!")  
+    } 
+   
