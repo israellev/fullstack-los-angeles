d78b0bca9d7d4c5d6df25aa50c7f800f3d6f855a
Sahar160 <saharbe1992@gmail.com>
2023-05-09 14:24:11 +0300

    some message

--- javascript/02 - TS, Operators, Conditions, Math/class-puzzle/sahar/index.ts
+++ javascript/02 - TS, Operators, Conditions, Math/class-puzzle/sahar/index.ts
@@ -1,3 +1,4 @@
+//puzzle 1
 let IQ = Number(prompt("what is your IQ level (put a number between 1 to 20)"))
 let gender = prompt("Are you a man or a woman?")
 if (gender === "man") {
@@ -17,4 +18,31 @@ if (gender === "man") {
     else {
         alert("אתה גאון חביבי!")  
     } 
+
+    //puzzle 2
+// program to check if the number is even or odd
+// take input from the user
+const number = Number(prompt("Enter a number: "));
+
+//check if the number is even
+if(number % 2 == 0) {
+   alert("The number is even.");
+  }
+
+// if the number is odd
+else {
+    alert("The number is odd.");
+}
+
+//puzzle 3
+alert("choose 3 numbers")
+const num1 = Number(prompt ("The first number is:"))
+const num2 = Number(prompt ("The second number is:"))
+const num3 = Number(prompt ("The third number is:"))
+
+const Result = alert ("The number is:" + Number(Math.max(num1, num2, num3)));
+
+
+
+
    
