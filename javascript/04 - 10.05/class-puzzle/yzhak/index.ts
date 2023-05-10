04ba89aceccd2df525223b0274c1090587bcf144
שרה דובה <sahalo57749@gmail.com>
2023-05-10 19:53:12 +0300

    somee

---
+++ javascript/04 - 10.05/class-puzzle/yzhak/index.ts
@@ -0,0 +1,13 @@
+export {};
+
+const aviadSpeed = Number(prompt("באיזה מהירות החללית של אביעד? (in km/h):"));
+
+const competitorSpeed = Number(prompt("מהירות החללית של המתחרה? (in km/h):"));
+
+if (aviadSpeed > competitorSpeed) {
+  console.log("אביעד ניצחח!");
+} else if (aviadSpeed < competitorSpeed) {
+  console.log("אביעד צריך למהר!!!");
+} else {
+  console.log("It's a tie!");
+}
