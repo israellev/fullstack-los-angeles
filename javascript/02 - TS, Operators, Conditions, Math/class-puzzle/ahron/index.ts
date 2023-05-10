92d410c16184a9e30944b66661f9d9d3b03b550e
unknown <ahrondawit@gmail.com>
2023-05-10 17:48:04 +0300

    some message

---
+++ javascript/02 - TS, Operators, Conditions, Math/class-puzzle/ahron/index.ts
@@ -0,0 +1,41 @@
+export{}
+console.log(Math.abs(-5)); // output: 5
+console.log(Math.ceil(4.2)); // output: 5
+console.log(Math.floor(4.8)); // output: 4
+console.log(Math.max(1, 3, 2)); // output: 3
+console.log(Math.min(1, 3, 2)); // output: 1
+console.log(Math.round(4.5)); // output: 5
+
+//puzzle 1
+let man
+let woman
+let IQ = Number(prompt("what is your IQ level ? (put a number between 1 to 20)"))
+let gender = prompt("Are you a man or a woman?")
+
+if (gender === man) {
+}
+
+else if{
+    (IQ < 10 ) 
+}
+    alert("אתה אהבל !")
+    if else{
+        alert('"אתה גאון")
+    }
+    
+     if (gender === woman )
+     
+
+else if{
+    (IQ < 10 ) 
+}
+    alert("את אהבל'ה !")
+    if else{
+        alert("את גאון")
+    }
+
+     
+
+
+        alert("את אהבל'ה !")
+        
