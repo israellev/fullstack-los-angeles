6cd69c0668972630f52c2cfcd6e775bfca6fcfcb
shir <shirmalka32@gmail.com>
2023-05-10 20:30:47 +0300

    some message

--- javascript/04 - 10.05/class-puzzle/shir/index.ts
+++ javascript/04 - 10.05/class-puzzle/shir/index.ts
@@ -1,17 +1,29 @@
 export {}
 
 
-const aviad = Number (prompt ( "הכנס את המהירות של החללית של אביעד"))
-const shir= Number (prompt ("הכנס את המהירות של החללית של שיר"))
+// const aviad = Number (prompt ( "הכנס את המהירות של החללית של אביעד"))
+// const shir= Number (prompt ("הכנס את המהירות של החללית של שיר"))
 
-if ( aviad>shir) {
-    alert ("אביעד ניצח במירוץ!")
-}
+// if ( aviad>shir) {
+//     alert ("אביעד ניצח במירוץ!")
+// }
 
-else if (aviad===shir) {
-    alert ( "התוצאה היא תיקו")
+// else if (aviad===shir) {
+//     alert ( "התוצאה היא תיקו")
+// }
+
+// else {
+//     alert ( "אביעד צריך להאיץ!")
+// }
+
+
+const panel= Number(prompt("נא הכנס/י את הכמות החשמל שפאנל סולרי מייצר ביום "))
+
+if ( panel %2 === 0 ) {
+
+    alert("מספר זה זוגי")
 }
 
 else {
-    alert ( "אביעד צריך להאיץ!")
+    alert ( "מספר זה אי זוגי")
 }
