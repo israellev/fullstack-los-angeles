67caa5751cf5752cbf8f3a2b1b16ad7d5eff409a
שרה דובה <sahalo57749@gmail.com>
2023-05-12 19:06:54 +0300

    some message

--- javascript/02 - TS, Operators, Conditions, Math/follow-up/yzhak/index.ts
+++ javascript/02 - TS, Operators, Conditions, Math/follow-up/yzhak/index.ts
@@ -3,3 +3,4 @@ console.log(Math.ceil(4.2)); // output: 5
 console.log(Math.floor(4.8)); // output: 4
 console.log(Math.max(1, 3, 2)); // output: 3
 console.log(Math.min(1, 3, 2)); // output: 1
+export {};
