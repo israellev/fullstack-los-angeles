b3f10546b4020d0b5678d21cd1db57d900233422
aviadkaduri <aviadkaduri121@gmail.com>
2023-05-15 20:00:47 +0300

    aviad

--- javascript/05 - 15.05/class-puzzle/aviad/index.ts
+++ javascript/05 - 15.05/class-puzzle/aviad/index.ts
@@ -1,6 +1,14 @@
+//class puzzle 1:
 export {};
 let birthday = 1
 while (birthday <= 23) {
     console.log("happy birthday" + birthday + " to Aviad")
     birthday++
+}
+
+//class puzzle 2:
+for(let day=1; day<=10; day++){
+    while (day % 2 == 0) {
+        console.log("daniel passed " + day + "days please water the plants")
+    }
 }
