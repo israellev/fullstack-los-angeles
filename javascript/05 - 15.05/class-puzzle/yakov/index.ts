e5297a7b2910d0ce02826c1acac1c28fbfcb10f0
yakovmangisto <yakov20286@gmail.com>
2023-05-15 19:55:32 +0300

    some message

--- javascript/05 - 15.05/class-puzzle/yakov/index.ts
+++ javascript/05 - 15.05/class-puzzle/yakov/index.ts
@@ -1,1 +1,15 @@
 export {};
+//3 ways to loop:
+for (let i = 1; i <= 23; i++) {
+  console.log(`${"happy birthday aviad your"} ${[i]} ${"yers old"}`);
+}
+
+// ---------------------------------------------------------------------
+// The Gardener's Plant Watering Schedule (use 'for')
+// Background: Daniel Michael Peretz works in gardening and needs to keep track of when to water the plants. Each plant needs to be watered every 2 days.
+// Exercise: Use a do while loop to create a schedule that tells Daniel which day he needs to water the plants over a 10 day period.
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 == 0)
+    console.log(`${"daniel passed"} ${[i]} ${"days please water the plants"}`);
+}
