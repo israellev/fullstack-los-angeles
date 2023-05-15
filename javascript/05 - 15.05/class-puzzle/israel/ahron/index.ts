bf75e906436abcfe1d27fe5136162afaa4c470e6
unknown <ahrondawit@gmail.com>
2023-05-15 19:40:08 +0300

    some message

---
+++ javascript/05 - 15.05/class-puzzle/israel/ahron/index.ts
@@ -0,0 +1,18 @@
+export{}
+for(let i =0 ; i <=10; i++){
+    console.log(i);
+}
+
+let days= 1 
+do {
+    console.log(days)
+    days ++
+} while (days <= 10)
+
+
+/////---------------
+
+let index = 0
+while (index < 10){
+
+}
