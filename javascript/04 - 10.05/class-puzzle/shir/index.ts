591d138aba7c0fbee094f6a0434455424233aae9
shir <shirmalka32@gmail.com>
2023-05-15 17:43:44 +0300

    some message

--- javascript/04 - 10.05/class-puzzle/shir/index.ts
+++ javascript/04 - 10.05/class-puzzle/shir/index.ts
@@ -1,5 +1,5 @@
 export {}
-
+// תרגיל 1:
 
 // const aviad = Number (prompt ( "הכנס את המהירות של החללית של אביעד"))
 // const shir= Number (prompt ("הכנס את המהירות של החללית של שיר"))
@@ -16,14 +16,174 @@ export {}
 //     alert ( "אביעד צריך להאיץ!")
 // }
 
+// תרגיל 2:
+// const panel= Number(prompt("נא הכנס/י את הכמות החשמל שפאנל סולרי מייצר ביום "))
 
-const panel= Number(prompt("נא הכנס/י את הכמות החשמל שפאנל סולרי מייצר ביום "))
+// if ( panel %2 === 0 ) {
 
-if ( panel %2 === 0 ) {
+//     alert("מספר זה זוגי")
+// }
 
-    alert("מספר זה זוגי")
+// else {
+//     alert ( "מספר זה אי זוגי")
+// }
+
+
+// תרגיל 1 לולאות: class puzzle
+
+// let floor= 1
+
+// for (let floor=1; floor <=10; floor++){
+
+// alert ("קומה מספר " + floor + " אושרה" )
+
+// }
+
+
+// תרגיל 2 לולאות: class-puzzle
+
+
+// for( let houer=1 ; houer <=12 ; houer++) {
+
+//   if ( houer <8) {
+
+//     alert ( `השעה עכשיו היא ${houer} יצחק לא צופה`)}
+
+//     else {
+//         alert (`השעה עכשיו היא ${houer} יצחק צופה`)
+//     }
+// }
+
+
+//home-puzzle 1
+// שאלות למורה: האם באמת היה צורך לציין את שני המשתנים? האם זה נכון מה שכתבתי במשתנה השני?
+// איך עושים את החלק האחרון בשאלה - של כמה פעמים הן אייתו נכון?
+// let day=1
+
+// let ansers= "true or false"?? 
+
+
+
+// for (let day=1; day<=10; day++) {
+
+//   const correct= Math.random() < 0.7 ;
+
+//   if( correct) {
+//     alert ( `ביום ${day} התלמידה אייתה בצורה נכונה`)
+//   }
+
+//   else {
+//     alert ( ` ביום ${day} התלמידה אייתה בצורה לא נכונה`)
+//   }
+
+
+// }
+//   alert( `התלמידים אייתו נכון את המילים ${correct} פעמים`)
+
+
+
+
+
+// // home puzzle 2 :  // האם המשתנה i הוא השעה?
+
+// let totalenergy= 1
+
+// for ( let houer = 6; houer <=18; houer++) {  
+
+//  let totalenergy = Math.random() < 10;   // למה הצאט עשה פה משהו אחר?
+
+// }
+
+// alert(`כמות האנרגיה שהופקה ליום היא ${ totalenergy}`) 
+
+
+
+
+// // התשובה של הצאט
+
+// let totalEnergy = 0;
+
+// for (let i = 6; i <= 18; i++) {
+//   const energy = Math.floor(Math.random() * 10) + 1;
+//   totalEnergy += energy;
+//   console.log(`At ${i}:00, the solar panel produced ${energy} kW of energy.`);
+// }
+
+// console.log(`Total energy produced for the day: ${totalEnergy} kW.`);
+
+
+
+
+
+
+// סתם נסיונות שלי
+// let i = 0
+// for (  let i=0; i<=10; i++) {
+//   if (i % 2 === 0){
+//   console.log ( `המספר ${ i} חיובי`)}
+// else
+// console.log (`המספר ${ i} שלילי`) 
+
+// // }
+
+
+
+// // 
+// let sh=0
+// let i= 0
+// for( i=0; i<=10; i ++) {
+
+//   if (i%2===0) {
+
+//     sh=i+i ;
+//   }
+// }
+
+// console.log ( sh);
+
+
+//
+
+// for (let i = 1; i <= 5; i++) {
+//   let asterisks = "";
+  
+//   for (let j = 1; j <= i; j++) {
+//     asterisks += "*";
+//   }
+
+//   console.log(asterisks);
+// }
+
+
+
+let=1
+for(let i=1; i<=5; i++) {
+  let cochav= " " ;
 }
 
-else {
-    alert ( "מספר זה אי זוגי")
+
+
+ console.log(cochav);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
