35514cb1640e5f2aa680b186fd9daa884a935f45
Mordechai <yonatan.mordechai@hp.com>
2023-05-15 19:59:11 +0300

    class

--- javascript/05 - 15.05/class-puzzle/yonatan/index.ts
+++ javascript/05 - 15.05/class-puzzle/yonatan/index.ts
@@ -1,1 +1,33 @@
 export {}
+// class puzzle:
+
+// // 1. The Birthday Celebrator (use 'while')
+// // Background: Aviad Kadouri is a former officer and enjoys celebrating birthdays. 
+// He wants a program that will print out a birthday message for each year up to his current age.
+// // Exercise: Use a while loop to print out a birthday message for each year up to Aviad's current age. 
+// Aviad is 23 years old.
+
+
+let birthday = 1;
+while (birthday <= 23) {
+
+    // alert("Happy" + birthday + "birthday Aviad")
+    console.log("Happy " + birthday + " birthday Aviad")
+    birthday++
+    
+}
+
+// 2. The Gardener's Plant Watering Schedule (use 'do while')
+// Background: Daniel Michael Peretz works in gardening and needs to keep track of when to 
+//water the plants. Each plant needs to be watered every 2 days.
+// Exercise: Use a do while loop to create a schedule that tells Daniel which 
+//day he needs to water the plants over a 10 day period.
+
+do {
+    water the plant
+    
+} while (didnt water in the last two days);
+
+
+// home puzzle:
+
