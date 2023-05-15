ae87a23634d2a0c9e7edd1068a2a2831b322602a
daniel <danielperetz620@gmail.com>
2023-05-15 20:23:13 +0300

    b.day

---
+++ javascript/05 - 15.05/class-puzzle/daniel/index.ts
@@ -0,0 +1,10 @@
+export {}
+for (let daniel =0; daniel <=23; daniel++){
+    let blessing = ""
+    if (daniel ==7) {
+        blessing = "yalla"
+    }
+    console.log("happy birthday daniel you are " +daniel +" years old now!" + blessing)
+    
+
+}
