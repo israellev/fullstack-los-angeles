85fcea08433d87e89fc8954e9a02a642a063b1de
yahaviva <124190354+yahaviva@users.noreply.github.com>
2023-05-15 20:37:35 +0300

    Yahaviva

--- javascript/05 - 15.05/class-puzzle/israel/index.ts
+++ javascript/05 - 15.05/class-puzzle/israel/index.ts
@@ -3,13 +3,8 @@ const AviadAge = 23;
 let i=0;
 while (i < AviadAge) {
     i++;
-    console.log("Happy Birthday Aviad, you are " + i + "years old");
+    console.log("Happy Birthday Aviad, you are " + i + " years old");
   }
   
-  
-const timePeriod = 10;
-let beginingDay = prompt("Please enter the date")
-for(let i=0; i<timePeriod; i=i+2 ){
-    console.log("Don't forget to water the garden");    
-}
+
 
