0867ea39964bbc7cd15c1c151aee1cad432f2a8a
isreallev <israellev@water-io.com>
2023-05-15 19:32:48 +0300

    some message

--- javascript/05 - 15.05/example/index.ts
+++ javascript/05 - 15.05/example/index.ts
@@ -1,39 +1,21 @@
 export {}
-
-//class-puzzle 1 from last lesson
-const gender = prompt("Are you a man or a woman?")
-// const IQ = Number(prompt("what is your IQ level ? (put a number between 1 to 20)"))
-
-// if (gender === "man") {
-//     if (IQ < 10) {
-//         alert("אתה אהבל !")
-//     } else {
-//         alert("אתה גאון")
-//     }
-// } else if (gender === "woman") {
-//     if (IQ < 10) {
-//         alert("את אהבלה !")
-//     } else {
-//         alert("את גאונה!")
-//     }
-// } else {
-//     alert("You are not currct!")
-// }
-
-///
-
-switch (gender) {
-    case "man":
-        console.log("man")
-        // if (IQ < 10) {
-        //     alert("אתה אהבל !")
-        // } else {
-        //     alert("אתה גאון")
-        // }
-        break;
-    case "woman":
-        console.log("woman")
-        break;
-    default:
-        console.log("a binary")
+/* 3 ways to loop: */
+/* OPTION 1 */
+for (let i = 0; i < 10; i++) {
+    console.log(i);
 }
+
+/*  OPTION 2 */
+let days = 1
+do {
+    console.log(days)
+    days ++
+} while (days <= 10)
+
+/* OPTION 3 */
+let index = 0
+while (index < 10) {
+    console.log(index)
+    index++
+}
+
