c50a26305d5ef23ec313f0108c12ac8f4778d10f
yakovmangisto <yakov20286@gmail.com>
2023-05-16 17:43:59 +0300

    some message

---
+++ javascript/05 - 15.05/home-puzzle/yakov/index.ts
@@ -0,0 +1,7 @@
+export {};
+// // 1. Greeting Function:
+// Write a function called greet that takes one parameter name. When called, the function should print a message saying "Hello, [name]!"
+
+function helloworld() {
+  console.log("ghhg");
+}
