1076d2481a0e55ac7be27403b0ed7683a623cefa
yakovmangisto <yakov20286@gmail.com>
2023-05-16 20:03:59 +0300

    some message

--- javascript/05 - 15.05/home-puzzle/yakov/index.ts
+++ javascript/05 - 15.05/home-puzzle/yakov/index.ts
@@ -1,11 +1,10 @@
 export {};
 // // 1. Greeting Function:
 // Write a function called greet that takes one parameter name. When called, the function should print a message saying "Hello, [name]!"
-const yourname = prompt("enter your name");
-function greet(yourname) {
-  console.log(`${"helow"} ${yourname}`);
+function greet(name) {
+  console.log("Hello, " + name + "!");
 }
-greet(yourname);
+greet("yakov");
 
 // 2. Square Function:
 // Write a function called square that takes one parameter num and returns the square of that number (num * num).
