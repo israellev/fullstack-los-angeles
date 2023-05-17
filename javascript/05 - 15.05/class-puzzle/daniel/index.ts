ef8fe7c530c911368ad3959a48c5aa48a30d7d0b
daniel <danielperetz620@gmail.com>
2023-05-17 18:15:57 +0300

    home

--- javascript/05 - 15.05/class-puzzle/daniel/index.ts
+++ javascript/05 - 15.05/class-puzzle/daniel/index.ts
@@ -1,10 +1,13 @@
 export {}
+// (1) this is loop//
 for (let daniel =0; daniel <=23; daniel++){
     let blessing = ""
     if (daniel ==7) {
         blessing = "yalla"
     }
     console.log("happy birthday daniel you are " +daniel +" years old now!" + blessing)
-    
+}
 
+for (let day =1; day<=10; day++){
+    
 }
---
+++ javascript/05 - 15.05/home-puzzle/daniel/index.ts
@@ -0,0 +1,10 @@
+export {}
+// age aviad
+for (let ageaviad=1; ageaviad<=23; ageaviad++){
+    console.log ("happy b day aviad hayakar! you are now" + ageaviad +"years old ")
+}
+// function grate 
+function name (grate){
+    console.log ("hey daniel hayakar"+grate+"!")
+}
+
