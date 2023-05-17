e4449484e11f70e69d1f3773a7571247f0178cb3
Brahanu Bayena <brahanu@trlst.local>
2023-05-17 18:41:16 +0300

    some message

--- javascript/05 - 15.05/home-puzzle/avi/index .ts
+++ javascript/05 - 15.05/home-puzzle/avi/index .ts
@@ -3,23 +3,23 @@ export {}
 
 // 1. Greeting Function:
 // Write a function called greet that takes one parameter name. When called, the function should print a message saying "Hello, [name]!"
-// const userName = prompt("Enter your name");
-// function greeting(userName){
-//     console.log(`${"hallo"} ${userName}`);
-// }
-// greeting(userName);
+const userName = prompt("Enter your name");
+function greeting(userName){
+    console.log(`${"hallo"} ${userName}`);
+}
+greeting(userName);
 
 
 // 2. Square Function:
 // Write a function called square that takes one parameter num and returns the square of that number (num * num).
 // use that function and print the result.
-const userNum = Number(prompt("Enter number"));
-function square(userNum){
-    const result = userNum*userNum;
-    return result;
-}
-const sum = square(userNum);
-console.log(`${"the result is : "} ${result}`);
+// const userNum = Number(prompt("Enter number"));
+// function square(userNum : number):number{
+//     const result = userNum*userNum;
+//     return result;
+// }
+// const sum = square(userNum);
+// console.log(`${"the result is : "} ${result}`);
 
 // 3. Temperature Conversion Function:
 // Write a function called convertToFahrenheit that takes a Celsius temperature as a parameter and returns the equivalent Fahrenheit temperature. The formula to convert Celsius to Fahrenheit is (celsius * 9/5) + 32.
