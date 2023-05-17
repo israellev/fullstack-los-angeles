542f4efb26a37ecb75d6fc2749cb42590ae1460d
shir <shirmalka32@gmail.com>
2023-05-17 17:36:20 +0300

    some message

---
+++ javascript/05 - 15.05/home-puzzle/shiru/index.ts
@@ -0,0 +1,19 @@
+export {}
+// home puzzle 1:
+
+function greet(shir) { 
+
+    console.log( `hellow ${shir}`);
+}
+
+greet("shir");
+greet("sarit");
+
+// home puzzle 2:
+
+function ribua(num) {
+
+
+}
+
+console.log( )
