57134b47527d698410b0f597cd47360e27c6a54c
שרה דובה <sahalo57749@gmail.com>
2023-05-17 20:03:53 +0300

    some message

---
+++ javascript/06 - 18.05/class-puzzle/yzhak/index.ts
@@ -0,0 +1,12 @@
+export {};
+// // Get full name
+// make anonymous function that get firstName and lastName and return full name.
+const getFullName = (firstName: string, lastName: string): string => {
+  const full = firstName + " " + lastName;
+  return full;
+};
+const firstName = prompt("your name is?");
+const lastName = prompt("Your last name is?");
+const fullName = getFullName(firstName, lastName);
+console.log(fullName);
+a;
