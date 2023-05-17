adcbe1dc558d85050b7737e33b0b050730f6f079
sapir peretz <sapsa1384@gmail.com>
2023-05-17 19:45:20 +0300

    sapir home puzzel

--- javascript/06 - 18.05/class-puzzle/sapir/index.ts
+++ javascript/06 - 18.05/class-puzzle/sapir/index.ts
@@ -1,1 +1,7 @@
 export {}
+let firastName = prompt("please enter first name :");
+let lastName = prompt("please enter last name name :");
+
+var fullName =(firstName:string, lastName:string):string => {
+    return firstName + " " + lastName
+}
