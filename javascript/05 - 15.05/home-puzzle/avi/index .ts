85b7761233d1487874b2242e4f70b0d8ef896820
Brahanu Bayena <brahanu@trlst.local>
2023-05-17 17:56:12 +0300

    some message

--- javascript/05 - 15.05/home-puzzle/avi/index .ts
+++ javascript/05 - 15.05/home-puzzle/avi/index .ts
@@ -15,9 +15,11 @@ greeting(userName);
 // use that function and print the result.
 const userNum = Number(prompt("Enter number"));
 function square(userNum){
-    console.log(`${"the result is : "} ${Math.pow(userNum,userNum)}`);
+    const result = Math.pow(userNum,2);
+    return result;
 }
-square(userNum);
+const sum = square(userNum);
+console.log(`${"the result is : "} ${result}`);
 
 // 3. Temperature Conversion Function:
 // Write a function called convertToFahrenheit that takes a Celsius temperature as a parameter and returns the equivalent Fahrenheit temperature. The formula to convert Celsius to Fahrenheit is (celsius * 9/5) + 32.
