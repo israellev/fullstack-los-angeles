2990886fd290205b5318044ddcf0d83526e1f150
sapir peretz <sapsa1384@gmail.com>
2023-05-19 15:51:51 +0300

    sapir home puzzel

--- javascript/06 - 18.05/class-puzzle/sapir/index.ts
+++ javascript/06 - 18.05/class-puzzle/sapir/index.ts
@@ -7,5 +7,5 @@ const GetfullName =(firstName:string, lastName:string):string => {
 }
 
 
-const fullName = GetfullName(firstName, lastName);
+const fullName = GetfullName(firstName, lastName);//מבצעת משתנה קבוע נוסף בשם "שם מלא" שמפעיל את הפונקציה "קבל שם מלא" שמקבל את המשתנים שם פרטי ושם משפחה ומדפיס אותו 
 console.log(fullName);
