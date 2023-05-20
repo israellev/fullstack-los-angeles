63f0c46d91166e1a2a5481b3a838d0f970e7c4bf
daniel <danielperetz620@gmail.com>
2023-05-20 16:10:37 +0300

    home work

--- javascript/06 - 18.05/class-puzzle/daniel/index.ts
+++ javascript/06 - 18.05/class-puzzle/daniel/index.ts
@@ -1,1 +1,8 @@
 export {}
+let firastName = prompt("please enter first name :");
+let lastName = prompt("please enter last name name :");
+
+
+const fullName =(firstName:string, lastName:string):string => {
+    return firstName + " " + lastName
+}
--- javascript/06 - 18.05/home-puzzle/israel/index.ts
+++ javascript/06 - 18.05/home-puzzle/israel/index.ts
@@ -1,1 +1,6 @@
 export {}
+//(1) Home - puzzle  
+function multiplicationTable(params:Number) {
+    for(let num =1; num <=10; num++)
+    console.log("the number now is"+num+"!")
+}
