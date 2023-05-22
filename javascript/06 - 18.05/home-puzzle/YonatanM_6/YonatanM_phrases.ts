df61284ab422c3ab255951d5636fdd84b0c24841
Mordechai <yonatan.mordechai@hp.com>
2023-05-22 17:23:30 +0300

    class

--- javascript/06 - 18.05/home-puzzle/YonatanM_6/YonatanM_phrases.ts
+++ javascript/06 - 18.05/home-puzzle/YonatanM_6/YonatanM_phrases.ts
@@ -14,41 +14,102 @@ export {}
 
 //  console.log(getFullName)
 
+//--------------------------------------------------------------------------------------------------------------------------//
 
 //  class puzzle:
 
 // 1. Get full name
 
 // make anonymous function that get firstName and lastName and return full name. 
-
-const getFullName = function(firstName, lastName) {
-        return `${firstName} ${lastName}`;
-      };
+// const getFullName = function(firstName, lastName) {
+//         return `${firstName} ${lastName}`;
+//       };
       
-      const firstName = prompt("Enter your first name please");
-      const lastName = prompt("Now enter your last name:");
+//       const firstName = prompt("Enter your first name please:");
+//       const lastName = prompt("Enter your last name please");
       
-      console.log(getFullName(firstName, lastName));
-      const fullName = getFullName(firstName, lastName);
-        alert(`Your full name is: ${fullName}`);
-// alert(`Your full name is : {getFullName}`); 
+//       const fullName = getFullName(firstName, lastName);
+//       console.log(fullName); // Output: what user inserted.
+      
+// 
+// למההשורה הזו לא עובדת, הרי התוכנה יודעת מה זהalert(`Your full name is : {getFullName}`); 
 
 // 2. Get the user age
-// ask the user by a function what is age, if is not number (isNaN) throw an error,
+// ask the user, using a function what his age is, if is not number (isNaN) throw an error,
 // use that function inside 'try catch'
 
+// const AgeCalcul = function() {
+//         const age = prompt("Enter your age:");
+      
+//         if (isNaN(age)) {
+//           throw new Error("Invalid input. Please enter numbers ONLY!.");
+//         }
+      
+//         return parseInt(age);
+//       };
+      
+//       try {
+//         const age = AgeCalcul();
+//         console.log("Age:", age);
+//       } catch (error) {
+//         console.log("Error:", error.message);
+//       }
+      
 // 3. 
 // re-write the #1 in variable function, and in arrow function
+// Variable function
 
+// const getFullName = function(firstName, lastName) {
+//         return `${firstName} ${lastName}`;
+//       };
+      
+//       const firstName = prompt("Enter your first name please");
+//       const lastName = prompt("Enter your last name please");
+      
+//       const fullName = getFullName(firstName, lastName);
+//       console.log(fullName); // Output: what user inserted
+      
 
 // home puzzle:
 
 // 1. Function and Loop - Multiplication Table:
-// Write a function named multiplicationTable that takes a parameter num. The function should print the multiplication table for that number from 1 up to 10 using a for loop. if the num is 5 it'll print for example '5 * 1 = 5', than '5 * 2 = 10', etc up to 10.
+// Write a function named multiplicationTable that takes a parameter num. 
+//The function should print the multiplication table for that number from 1 up to 10 using a for loop. 
+//if the num is 5 it'll print for example '5 * 1 = 5', than '5 * 2 = 10', etc up to 10.
+
+// function multiplicationTable(num:number) {
+//         for (let i = 1; i <= 10; i++) {
+//           const result = number * i;
+//           console.log(`${number} x ${i} = ${result}`);
+//         }
+//       }
+      
+// let calculation = prompt("Type the number you want multiplied")
+// multiplicationTable(calculation);
+      
 
 // 2. Countdown Timer:
 // Write a function that asks the user for a number and then counts down from that number to zero, printing each number on a new line. Use a for loop to accomplish this.
 // If the user enter something not number - throw error.
+
+function countdown() {
+        const number = parseInt(prompt("Enter a number please:"));
+      
+        if (isNaN(number)) {
+          throw new Error("Invalid input. Please enter a number.");
+        }
+      
+        for (let i = number; i >= 0; i--) {
+          console.log(i);
+        }
+      }
+      
+      try {
+        countdown();
+      } catch (error) {
+        console.log("Error:", error.message);
+      }
+      
 
 // 3. Bonus - Palindrome Checker:
 // Create a function that asks the user for a string and checks whether the string is a palindrome or not. A palindrome is a word, phrase, number, or other sequence of characters that reads the same backward or forward. Note: This challenge must be accomplished without using array methods.
