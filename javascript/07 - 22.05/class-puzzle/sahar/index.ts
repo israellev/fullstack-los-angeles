27bba8ebba7bc1a04694f6417be33890057fbf37
Sahar160 <saharbe1992@gmail.com>
2023-05-22 23:01:41 +0300

    some message

--- javascript/07 - 22.05/class-puzzle/sahar/index.ts
+++ javascript/07 - 22.05/class-puzzle/sahar/index.ts
@@ -1,8 +1,25 @@
-export{}
+export { }
 
-function printState(string) {
-    const content = [string]
-        content [0]
-        content [content.length-1]
+//puzzle 1
+function printState(str) {
+    console.log(str[0]);
+    console.log(str[str.length - 1]);
 }
 printState("lion")
+
+//puzzle 2
+function stringValidation() {
+    const str = prompt("Enter the letters A in any order")
+    if (str == "a")
+        console.log(str.trim().toLowerCase().replace(/a/g, "b"))
+    else {
+        throw "Error"
+    }
+}
+
+try {
+    stringValidation()
+}
+catch (Error) {
+    alert(Error)
+}
