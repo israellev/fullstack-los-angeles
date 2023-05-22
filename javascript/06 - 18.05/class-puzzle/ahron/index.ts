75780b9245bbbbf9dd4082d2c0e7528310023f59
unknown <ahrondawit@gmail.com>
2023-05-22 21:22:57 +0300

    some message

--- javascript/06 - 18.05/class-puzzle/ahron/index.ts
+++ javascript/06 - 18.05/class-puzzle/ahron/index.ts
@@ -34,3 +34,16 @@ alert (c)
 }
 
 console.log("The code is continue");
+ 
+//  Function and Loop - Multiplication Table:
+// Write a function named multiplicationTable that takes a parameter num.
+//  The function should print the multiplication table for that number from 1 up to 10 using a for loop. if the num is 5 it'll print for example '5 * 1 = 5', than '5 * 2 = 10', etc up to 10.
+
+// function multiplicationTable(num) {
+//   for (let i = 1; i <= 10; i++) {
+//     const result = num * i;
+//     console.log(`${num} * ${i} = ${result}`);
+//   }
+// }
+
+// multiplicationTable(5);
