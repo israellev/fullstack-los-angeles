bd00036eae62da6f1531f40266b877c786963ce7
yakovmangisto <yakov20286@gmail.com>
2023-05-22 21:22:29 +0300

    some message

--- javascript/07 - 22.05/class-puzzle/yakov/index.ts
+++ javascript/07 - 22.05/class-puzzle/yakov/index.ts
@@ -12,3 +12,4 @@ printState("yakov");
 // 2. Create a function named stringManipulation that takes in a string, removes whitespace from both ends, converts it to lower case and then replaces all occurrences of the letter 'a' with 'x'. Make sure the function throws an error if something other than a string is input
 
 // home puzzle:
+function stringManipulation(name: string) {}
