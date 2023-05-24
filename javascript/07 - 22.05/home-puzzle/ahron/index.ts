20e920065d0bab1f7cfd340d68b00623e00f474d
unknown <ahrondawit@gmail.com>
2023-05-24 17:51:54 +0300

    some message

--- javascript/07 - 22.05/home-puzzle/ahron/index.ts
+++ javascript/07 - 22.05/home-puzzle/ahron/index.ts
@@ -1,73 +1,36 @@
-export{}
+ export{}
 
-// // ברשימה  פונקציה שמדפיסה את המילה ההראשונה והאחרונה 
 
-// function printFirstAndLast(str){
-// const firstName=str[0];
-// const lastName =str[str.length -1 ];
-// console.log (firstName);
-// console.log (lastName);
+// home puzzle:
+
+// 1. Sum the array
+// Write a function that receive array of numbers (only) as a parameter, 
+// and return the result of all numbers together.
+// for example from [1, 2, 3] it returns 6
+// // 
+// function sumOfArray (numbers:number[]):number{
+//   let sum : number = 0;
+//   for(let i: number = 0; i < numbers.length ; i++ ){
+//     sum += numbers [i]; 
+//   }
+//   return sum;
 // }
 
-// // קריאה לפונקציה 
- 
-// function printstate(str){
-//   console.log  ([0],[str.length -1]);
-// }
+// let numbers:number[]= [13,10,91];
+// let result :numbers = sumOfArray([numbers]);
+// console.log(`the sum of the array numbers are: ${result}`);
 
 
 
-// const arr = [`w`,`e`,`w`,`f`]
-// printstate(arr); 
-// printFirstAndLast(arr);
+// 2. max number in Array 
+// Write a function that recive an array of numbers and returns the bigget number index.
+// for example from [1, 2, 3] it returns 2 (the index of 3).
 
+// 3. avg number in array
+// Write a function that recive an array of numbers and returns the average number.
+// for example from [1, 2, 3] it returns 2.
 
+// 4. check if array contains the item
+// Write a function that recive an array and value, return true if the value exists in the array, and false if not.
+// for example from ([1, 2, 3], 4) it returns false.
 
-// // 2.string validation
-// // Create a function that takes in a string, removes whitespace from both ends, converts it to lower case and then replaces all occurrences of the letter 'a' with 'b'. Make sure the function throws an error if something other than a string is input
-// // for example from " aaa " it will returs "bbb".
-
-
-
-// // function validate (){
-// //   const validation:string = prompt (`How are you ? `); 
-// //   if( validation !==  `string`) {
-// // throw Error(`input error`);
-
-// //   }
-// // const validationtrim= validation.trim();
-// // const toLowerCasevali = validationtrim.toLowerCase();
-// // const transfromstring = toLowerCasevali.replace ( /a/g, `b`)
- 
-// // return console.log (transfromstring)
-
-// // }
-
-// //  validate ()
-
-
- 
-// // 3. create dynamic array 
-// // Creat a function that received a 'num' and return array of numbers 
-// // that start from 0 to the number of the 'num'. 
-// // for example user enter 3, the function return [0, 1, 2, 3], etc. 
-
- function dynamicArray (num) {
-const array = [] ;
-
-for (let i= 0; i<=num; i++){
-array.push(i) ;
-} 
-
-return array
-}
-
- const num = 3;
-
- const result = dynamicArray(num);
- console.log (result);
-
-
-
- dynamicArray(7);
- console.log ();
