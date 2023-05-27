f780d3ca8aad646d0ddd045f7a12e5cd2f657786
yakovmangisto <yakov20286@gmail.com>
2023-05-27 16:40:44 +0300

    some message

--- javascript/02 - TS, Operators, Conditions, Math/class-puzzle/yakov/index.ts
+++ javascript/02 - TS, Operators, Conditions, Math/class-puzzle/yakov/index.ts
@@ -5,3 +5,4 @@ console.log(Math.floor(4.8)); // output: 4
 console.log(Math.max(1, 3, 2)); // output: 3
 console.log(Math.min(1, 3, 2)); // output: 1
 console.log(Math.round(4.5)); // output: 5
+var a = "ya";
