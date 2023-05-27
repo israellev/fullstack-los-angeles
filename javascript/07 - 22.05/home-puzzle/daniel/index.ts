e10e45012e9e8e4c312eaff860462f96949f99d7
daniel <danielperetz620@gmail.com>
2023-05-28 00:22:03 +0300

    Home  pazzle

--- javascript/07 - 22.05/home-puzzle/daniel/index.ts
+++ javascript/07 - 22.05/home-puzzle/daniel/index.ts
@@ -1,11 +1,41 @@
-export {}
-// First Letter
+export { }
+// (5) First Letter
 // Write a funtion that gets array of names and return array with first letter of those names in upper case.
 // for example from ["liad", "sarit"] it returns ["S", "L"]
 
-function capitalizeFirstLetter(arrNsme:Name[]):Name{
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+
+
+
+// function getFirstLetter(names: string[]): string[] {
+//     const  firstLetters: string [] = [];
+//     for (let i = 0; i < names.length; i++) {
+//         const name = names[i];
+//         const firstLetter = name.charAt(0).toUpperCase();
+//         firstLetters.push(firstLetter);
+//     }
+//     return firstLetters;
+// }
+// const names = ["liad","sarit"];
+// const result = getFirstLetter(names);
+// console.log(result);
+
+
+
+// 6. Capital Letters
+// Write a funtion that gets array of names and return array with those names in capital letter.
+// for example from ["liad", "sarit"] it returns ["Liad", "Sarit"]
+// (I already showed)
+
+function capitalizeNames(names: string[]): string[] {
+    const capitalLetters: string[] = [];
+    for (let i = 0; i < names.length; i++) {
+        const element = names[i];
+        const capLetters = element.toUpperCase();
+        capitalLetters.push(capLetters);
+    }
+
+    return capitalLetters;
 }
-}
+const names = ["daniel", "liad", "ahron", "yehonatan"]
+const result1 = capitalizeNames(names);
+console.log(result1);
