ee207549f9341db5b7552861b61e782cdca6cb28
yakovmangisto <yakov20286@gmail.com>
2023-05-27 19:30:24 +0300

    some message

--- javascript/04 - 10.05/class-puzzle/yakov/index.ts
+++ javascript/04 - 10.05/class-puzzle/yakov/index.ts
@@ -1,25 +1,26 @@
-export {}
+export {};
 
-//class-puzzle 1 from last lesson
-// const gender = prompt("Are you a man or a woman?")
-// const IQ = Number(prompt("what is your IQ level ? (put a number between 1 to 20)"))
+// class-puzzle 1 from last lesson
+const gender = prompt("Are you a man or a woman?");
+const IQ = Number(
+  prompt("what is your IQ level ? (put a number between 1 to 20)")
+);
 
-// if (gender === "man") {
-//     if (IQ < 10) {
-//         alert("אתה אהבל !")
-//     } else {
-//         alert("אתה גאון")
-//     }
-// } else if (gender === "woman") {
-//     if (IQ < 10) {
-//         alert("את אהבלה !")
-//     } else {
-//         alert("את גאונה!")
-//     }
-// } else {
-//     alert("You are not currct!")
-// }
-
+if (gender === "man") {
+  if (IQ < 10) {
+    alert("אתה אהבל !");
+  } else {
+    alert("אתה גאון");
+  }
+} else if (gender === "woman") {
+  if (IQ < 10) {
+    alert("את אהבלה !");
+  } else {
+    alert("את גאונה!");
+  }
+} else {
+  alert("You are not currct!");
+}
 
 // // switch (gender) {
 //     case "man":
@@ -36,7 +37,6 @@ export {}
 //     default:
 //         console.log("a binary")
 // }
-
 
 console.log(Math.abs(-5)); // output: 5
 console.log(Math.ceil(4.2)); // output: 5
