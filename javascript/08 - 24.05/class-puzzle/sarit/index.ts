a4f22c718b6c54434469b9a508f0f7cc0fc76913
Sarit <saritts92@gmail.com>
2023-05-29 21:16:53 +0300

    another

---
+++ javascript/08 - 24.05/class-puzzle/sarit/index.ts
@@ -0,0 +1,26 @@
+export {}
+
+// 1. Doubling an Array of Numbers- Create a function named squareNumbers that takes an array of numbers and returns a new array with the square of each number. 
+// Use the map function to achieve this. example: from [1, 2, 3, 4, 5] it returns [2, 4, 6, 8, 10]
+
+// function squareNumbers(numbers: number[]): number[] {
+//     const squareNumbers= numbers.map(number => number * number);
+//     return squareNumbers;
+// }
+
+// const squareInput= [20, 10, 5, 30, 2];
+// const result= squareNumbers(squareInput);
+// console.log(result);
+
+// 2. Convert list of names to capital letters
+// Create a function named capitalNames that takes an array of strings and returns a new array with same string in capital first letter. Use the map function to achieve this.
+// example: from ["john", "smith"] it returns ["John", "Smith"]
+
+function capitalNames(names: string[]): string[] {
+   const capitalLetters = names.map(name => name.charAt(0).toUpperCase() + name.slice(1));
+return capitalLetters;
+}
+
+const inputNames = ["sarit", "yuval", "chana", "shon"];
+const result= capitalNames(inputNames);
+console.log(result);
