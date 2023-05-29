7826d860d89178b89574b4c7fca68db2dcd65994
unknown <ahrondawit@gmail.com>
2023-05-29 21:01:34 +0300

    some message

---
+++ javascript/08 - 24.05/home-puzzle/ahron/index.ts
@@ -0,0 +1,12 @@
+ export{}
+//  1. Doubling an Array of Numbers
+//  Create a function named squareNumbers that takes an array of numbers and returns a new array with the square of each number. Use the map function to achieve this.
+//  example: from [1, 2, 3, 4, 5] it returns [2, 4, 6, 8, 10]function squareNumbers(numbers) {
+
+function squareNumbers(arr:number[]){
+  const newsquar=arr.map((num)=>num**2)
+  return newsquar
+};
+
+const square = [1,2,3,4,5];
+console.log(squareNumbers(square));
