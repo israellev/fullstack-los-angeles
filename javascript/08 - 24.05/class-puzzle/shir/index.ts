7100c34666886a648d507441ce58e9119e0b6038
isreallev <israellev@water-io.com>
2023-05-29 21:05:56 +0300

    some message

--- javascript/08 - 24.05/class-puzzle/shir/index.ts
+++ javascript/08 - 24.05/class-puzzle/shir/index.ts
@@ -1,13 +1,13 @@
 export {}
 
-function squareNumbers(numbers: number[]): number[] {
+// function squareNumbers(numbers: number[]): number[] {
+//     return numbers.map(nu => nu * nu)
+// }
+
+const squareNumbers = (numbers:number[]): number[] => {
     return numbers.map(nu => nu * nu)
 }
-
-// const squareNumbers = (numbers:number[]) => numbers.map(nu => nu * nu)
-
 
 const arr = [1,2,3];
 console.log(arr);
 console.log(squareNumbers(arr));
-
