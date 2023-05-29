8959bba6a04cb9dcee53ace73d1b443d50cf4016
Mordechai <yonatan.mordechai@hp.com>
2023-05-29 13:45:02 +0300

    class

--- javascript/07 - 22.05/home-puzzle/yonatanM/index.ts
+++ javascript/07 - 22.05/home-puzzle/yonatanM/index.ts
@@ -40,9 +40,70 @@ export {}
 // for example from ([1, 2, 3], 4) it returns false.
 // (arr: number[], num: number) => boolean
 
+function Checker(arr, value: number[]): boolean {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === value)
+      return true;
+    }
+    return false;
+  }
+  
+  const arr[] = prompt("enter a list of numbers") 
+  
+  console.log(result); // Output: total average
+  
+//   this is how chatGPT gets an array from the user using prompt, and than user inserts a IDBCursorWithValue
+//   and we check
+  function checkValueInArray() {
+    // Get user input for the array
+    var arrInput = prompt("Enter numbers separated by commas:");
+  
+    // Convert the input string into an array
+    var arr = arrInput.split(",").map(Number);
+  
+    // Get user input for the value to check
+    var value = Number(prompt("Enter a number to check:"));
+  
+    // Check if the value exists in the array
+    var result = arr.includes(value);
+  
+    // Output the result
+    if (result) {
+      alert("The value exists in the array!");
+    } else {
+      alert("The value does not exist in the array.");
+    }
+  }
+  
+  // Call the function to start the process
+  checkValueInArray();
+  
+
 // 5. First Letter
 // Write a funtion that gets array of names and return array with first letter of those names in upper case.
 // for example from ["liad", "sarit"] it returns ["S", "L"]
+
+function capitalizeName(arr) {
+    /*
+    Function to capitalize the first letter of each name in an array.
+  
+    Parameters:
+    arr (array): The input array containing names.
+  
+    Returns:
+    array: An array of names with the first letter capitalized.
+    */
+    var capitalizedNames = arr.map(function(name) {
+      return name.charAt(0).toUpperCase() + name.slice(1);
+    });
+  
+    return capitalizedNames;
+  }
+
+var names = ["john", "emma", "peter"];
+
+var capitalizedNames = capitalizeName(names);
+console.log(capitalizedNames);  // Output: ["John", "Emma", "Peter"]
 
 // 6. Capital Letters
 // Write a funtion that gets array of names and return array with those names in capital letter.
