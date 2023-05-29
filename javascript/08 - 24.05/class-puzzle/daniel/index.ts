56dbd88d767b1137d5caf7500d317875155ef081
daniel <danielperetz620@gmail.com>
2023-05-29 21:07:02 +0300

    metodot

--- javascript/08 - 24.05/class-puzzle/daniel/index.ts
+++ javascript/08 - 24.05/class-puzzle/daniel/index.ts
@@ -1,1 +1,14 @@
-export {}
+export { }
+//  class puzzle 
+// 1. Doubling an Array of Numbers
+// Create a function named squareNumbers that takes an array of numbers and returns a new array with the square of each number. Use the map function to achieve this.
+// example: from [1, 2, 3, 4, 5] it returns [2, 4, 6, 8, 10]
+
+function squareNumbers(arr:number[]):number[] {
+  return arr.map((n)=> n*n)
+}
+
+const arr =[1, 2, 3, 4, 5];
+const daniel = squareNumbers(arr)
+console.log(daniel)
+
