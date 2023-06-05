e2e193f24a5b84b3e3c1ea1e08a5ed10257c30c1
Mordechai <yonatan.mordechai@hp.com>
2023-06-05 17:08:14 +0300

    class

---
+++ javascript/09 - 31.05/class-puzzle/yonatans/index.ts
@@ -0,0 +1,45 @@
+export {}
+// Class puzzle - Practice arrys:
+
+// 1. Filter out non-string types and trim whitespace (map)
+
+// Write a function that accepts an array of different types of data. It should first use the filter function to keep only the elements that are of type string, and then use map to return a new array where each string has been trimmed of whitespace using the trim() function.
+// for example from [" banana", 1, "apple "] it return ["banana", "apple"]
+
+// 2. function ReplaceItemInArray (map)
+// Write a function reaplaceItem that:
+// a. find the 'itemToReplace' in the array, and replace it with 'replaceWith',
+// b. return the new array.
+// (arr: sting[], itemToReplace: string, replaceWith: string): string[]
+// for example from itemToReplace(["shir", "yakov"], "yakov", "sapir")
+// it will return ["shir", "sapir"].
+
+// 3. function getFirstAndLast:
+// from example from [1, 2, 3] it'll return [1, 3] (the first and last item only)
+// than use extraction to save 1 and 2 to "first" and "last" variables.
+
+
+
+// home puzzle:
+
+
+
+
+
+
+
+
+
+
+
+
+// Bonuses - arrays:
+
+// 1. Implementing ForEach:
+// Implement a function myForEach that takes an array and a callback function and behaves like the forEach method. It should apply the callback function to each element in the array.
+
+// 2. Creating Custom Filter Function:
+// Implement your own filter function named myFilter. It should take an array and a callback function that returns a boolean. myFilter should return a new array containing only the elements of the original array for which the callback function returns true.
+
+// 3. Creating Custom Filter Function:
+// Implement your own filter function named myFilter. It should take an array and a callback function that returns a boolean. myFilter should return a new array containing only the elements of the original array for which the callback function returns true.
