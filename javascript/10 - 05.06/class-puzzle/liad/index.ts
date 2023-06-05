80983e5466dabc064b4014d09d60ccf232bf5dc6
Liad <yovevisami@gmail.com>
2023-06-05 19:58:11 +0300

    jjjj

---
+++ javascript/10 - 05.06/class-puzzle/liad/index.ts
@@ -0,0 +1,15 @@
+export {}
+
+const id = {
+
+Name : "liad" ,
+age : 24 ,
+Dateofbirth : 24/09/1999 ,
+Weight : 74 
+
+};
+
+console.log("name" + id.Name);
+console.log("age" + id.age);
+console.log("Dateofbirth" + id.Dateofbirth);
+console.log("Weight" + id.Weight);
