0608a8d55bba8b1d3bbd9d8f19c8ffbc5401a4c8
isreallev <israellev@water-io.com>
2023-06-05 21:12:15 +0300

    some message

--- javascript/10 - 05.06/example/index.ts
+++ javascript/10 - 05.06/example/index.ts
@@ -20,15 +20,18 @@ const arr = [1,2,"" ,]
 const person: any = {
     name: "sapir",
     age: 30,
-    address: null,
+    address: {
+        city: "rechovot"
+    },
     hobbies: ["fishing", "diving"],
     studies: {
         schoolName: "tichon"
     },
     isMarried: false,
-    celebrateBirthday: function() {
+    celebrateBirthday: function(greeting) {
         this.age ++
-    }
+        console.log(greeting)
+    },
 }
 
 /* get values */
@@ -52,3 +55,82 @@ console.log(person.age) // 31
 
 
 
+/* distracting object */
+
+const person2 = person
+person2.age // 31
+person2.celebrateBirthday()
+person2.age // 32
+person.age // 32
+
+// const age = Number(prompt("give me your age")) // 44
+
+// const person3 = {
+//     ...person, // extrating all
+//     name: name,
+//     age, // shortcut to 'age: age'
+// }
+
+// console.log(age) // 44
+// console.log(person3.name) // "Boaz"
+// console.log(person3.celebrateBirthday("Mazal Tov!!"))
+// console.log(person3.age) // 45
+
+
+/* use example:
+const classList = [person, person, person3]
+classList.forEach(student => {
+    console.log(student.age)
+}) */
+
+/*   */
+// instead of:  
+// do:
+
+const book = {
+    title: "Afternoon stories",
+    author: "Nick Bossi",
+    price: 28,
+}
+
+
+const books = [
+book, 
+{
+    title: "Night stories",
+    author: "Nick Bossi",
+    price: 33,
+}, {
+    title: "Morning stories",
+    author: "Nick Bossi",
+    price: 45,
+}]
+
+books[0].title // "Afternoon stories"
+books[1].title // "Night stories"
+
+function printTitleAndPrice(listBooks) {
+    listBooks.forEach(booky => {
+        const {title, price, noooooo} = booky
+        console.log(`Title: ${title}, price: ${price}`)
+    })   
+}
+
+console.log(books)
+printTitleAndPrice(books)
+
+
+function allBookPrice(books): number {
+    let sumPrice = 0;
+    books.forEach(book => {
+        sumPrice += book.price
+    })
+    return sumPrice;
+}
+
+console.log(allBookPrice(books))
+
+const prices = books.map(book => book.price)
+console.log(prices)
+const expeniveBooks = books.filter(book => book.price > 30)
+console.log(expeniveBooks)
--- javascript/10 - 05.06/puzzles.md
+++ javascript/10 - 05.06/puzzles.md
@@ -14,6 +14,22 @@ Add a property isRead to your book object (set to false initially). Then add a m
 
 class puzzle:
 
+1. Object Key-Value Printing
+Create a function that accepts an object and prints out each key-value pair in the format: key: value.
+
+2. Object Array Manipulation
+Define an array of objects, each representing a person with properties "name" and "age". Use forEach to print each person's name and age, map to create an array of their ages, and filter to create a new array that only contains people older than 20.
+
 
 home puzzle:
+
+1. Unique Value Extraction
+Given an array with duplicate values, use the Set object to remove the duplicates. Then convert the Set back into an array and print it.
+
+
+
+Bonus:
+1. Get average of list of objects
+Create a function getAverageScors that gets array of object with 'score' value, and return the average class score.
+(arrOfObj): number
 
