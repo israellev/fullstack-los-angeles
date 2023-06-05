8a6ed8ff825fab5b2e18a2d771e90f25806a7958
Mordechai <yonatan.mordechai@hp.com>
2023-06-05 19:37:06 +0300

    class

--- javascript/10 - 05.06/class-puzzle/yonatan/index.ts
+++ javascript/10 - 05.06/class-puzzle/yonatan/index.ts
@@ -1,1 +1,33 @@
 export {}
+
+
+Objects follow-up:
+
+1. Object Creation
+Create an object representing a book, 
+which has properties like title, author, yearPublished and pages. 
+Then, print each property using dot notation.
+
+const book1 = {
+
+    title = New houses;
+    author = israel lev;
+    yearPublished = 2002;
+    numOfPages = 101;
+
+}
+
+const book2 = {
+
+    title = old houses;
+    author = ishay lev;
+    yearPublished = 2000;
+    numOfPages = 99;
+
+}
+
+2. Object Methods
+Continuing from the previous exercise, add a method getSummary to the book object, which when called, returns a summary of the book as a string: "title, written by author in yearPublished, contains pages pages".
+
+3. Modifying Object Properties
+Add a property isRead to your book object (set to false initially). Then add a method readBook to the book object which changes the value of isRead to true.
