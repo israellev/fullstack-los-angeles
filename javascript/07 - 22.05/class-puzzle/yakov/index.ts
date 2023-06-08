9e7cc1da8ed63014af8d27c8e881b35f013c434d
yakovmangisto <yakov20286@gmail.com>
2023-06-08 16:56:59 +0300

    sdf

--- javascript/07 - 22.05/class-puzzle/yakov/index.ts
+++ javascript/07 - 22.05/class-puzzle/yakov/index.ts
@@ -1,63 +1,50 @@
 export {};
+
+// const arr = ["apple", "orange", "strawberry", 1, 2, true];
+// arr[0];
+// arr[arr.length - 1];
+// arr.indexOf("orange");
+// console.log;
+
 // class puzzle:
-// 1. Write a function that calls 'printState' that get 'str' parameter and print the fisrt and last string.
 console.log("class puzzle: 1------------------");
-// function printState(name: string) {
-//   // b=input.length[]
-//   console.log(name);
-//   console.log(name[0]);
-//   console.log(name[name.length - 1]);
-// }
-// printState("yakov");
+// 1. Write a function that calls 'printState' that get 'str' parameter and print the fisrt and last string.
+function printState(name: string) {
+  console.log(name);
+  console.log(name[0]);
+  console.log(name[name.length - 1]);
+}
+printState("yakov");
 
-// 2. Create a function named stringManipulation that takes in a string, removes whitespace from both ends, converts it to lower case and then replaces all occurrences of the letter 'a' with 'x'. Make sure the function throws an error if something other than a string is input
 console.log("class puzzle: 2------------------");
-// function stringValidation(string: string):void {
-//   const streingg = prompt("please enter a string:");
-//   if (isNaN(Number(streingg)))
-//     throw  Error("oops! you didnt enter a string");
-//   else {
-//     let chng1 = streingg.replace(/^\s+|\s+$/gm, "");
-//     let chng2 = chng1.toLowerCase();
-//     let chng3 = chng2.replace(/a/g, "x");
-//     console.log(chng3);
-//   }
-// }
+// // 2. Create a function named stringManipulation that takes in a string, removes whitespace from both ends,
+//  converts it to lower case and then replaces all occurrences of the letter 'a' with 'b'.
+//  Make sure the function throws an error if something other than a string is input
 
-//    try {
-//        stringValidation()
-//    }
-//    catch (Error) {
-//        alert(Error)
-//  }
-
-// function stringValidation() {
-//   const str = prompt("Enter the letters ");
-//   if (!isNaN(Number(str))) throw "Error";
-//   else {
-//     console.log(str.trim().toLowerCase().replace(/a/g, "b"));
-//   }
-// }
-
-// try {
-//   stringValidation();
-// } catch (Error) {
-//   alert(Error);
-// }
-
+function stringManipulation(strr: string) {
+  if (typeof strr !== "string") {
+    throw new Error("נא הכניס מילה!");
+  }
+  let str1 = strr.trim();
+  let str2 = strr.toLowerCase();
+  let str3 = strr.replace(/a/g, "b");
+  console.log(str3);
+}
+const input = prompt("בבקשה הכנס את שמך");
+stringManipulation(input);
 // 3. create dynamicArray
 // Creat a function that received a 'num' and return array of numbers
 // that start from 0 to the number of the 'num'.
 // for example user enter 3, the function return [0, 1, 2, 3], etc.
 // (num: number) => number[]
 
-// console.log("class puzzle: 3------------------");
-
-// function createNumberArray(num: number): Array<number> {
-//   let numberArray = [];
-//   for (var i = 0; i <= num; i++) {
-//     numberArray.push(i);
-//   }
-//   return numberArray;
-// }
-// console.log(createNumberArray(5));
+console.log("class puzzle: 3------------------");
+function arrayofnumbers(num: number) {
+  let newarr = [];
+  for (let i = 0; i <= num; i++) {
+    newarr.push(i);
+  }
+  return newarr;
+}
+let arrr = arrayofnumbers(15);
+console.log(arrr);
