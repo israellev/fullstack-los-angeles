eb5206aa67e5cc7264d0b41411fbf40c389bcbb4
yakovmangisto <yakov20286@gmail.com>
2023-06-12 19:43:48 +0300

    sdf

---
+++ javascript/12 - 12.06/class-puzzle/yakov/index.ts
@@ -0,0 +1,12 @@
+export {};
+
+// class puzzle:
+import { students } from "../../../11 - 07.06/home-puzzle/yakov";
+// 1. print in the console for each student in the list using Object.keys():
+// key: value
+
+let key = Object.keys(students);
+console.log(students);
+// keys.forEach(key => {
+//   console.log(`${key}: `, person[key])
+// })
