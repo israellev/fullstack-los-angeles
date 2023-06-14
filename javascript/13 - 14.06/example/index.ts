5fb2f58e1209d725ff5ad67e5c6be7d06ca8ee76
isreallev <israellev@water-io.com>
2023-06-14 20:06:32 +0300

    some message

--- javascript/13 - 14.06/example/index.ts
+++ javascript/13 - 14.06/example/index.ts
@@ -17,10 +17,41 @@ headers = {}
 // @ts-ignore
 fetch("https://jsonplaceholder.typicode.com/posts", {method: "GET"})
 .then(res => res.json())
-.then(res => {
-    console.log(res)
-    console.log(res[0])
-    
+.then((res: any[]) => {
+    res.reverse().forEach(post => {
+        // {
+        //     "userId": 1,
+        //     "id": 1,
+        //     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        //     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        //   }
+        const htmlPost = `
+        <div class="card mb-4" id="post-${post.id}">
+            <div class="card-header">
+                <h5 class="card-title">${post.id} - ${post.title}</h5>
+                <small class="text-muted">Posted by User ${post.userId}</small>
+            </div>
+            <div class="card-body">
+                <p class="card-text">${post.body}</p>
+                <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#comments-1" aria-expanded="false" aria-controls="comments-1">
+                Show comments
+                </button>
+                <div class="collapse" id="comments-1">
+                <!-- Comments will be dynamically added here -->
+                </div>
+            </div>
+        </div>
+        `;
+        const newDiv = document.createElement("div")
+        newDiv.innerHTML = htmlPost
+        document.getElementById("postContainer").appendChild(newDiv)
+    })
+
+
+
+   
+
+
 })
 .catch(error => console.log(error))
 
