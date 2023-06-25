15bf319408d4122d554e9bc5b69b9ebfed2114b1
yakovmangisto <yakov20286@gmail.com>
2023-06-25 21:28:35 +0300

    sdf

--- javascript/13 - 14.06/home-puzzle/yakov/index.ts
+++ javascript/13 - 14.06/home-puzzle/yakov/index.ts
@@ -4,13 +4,32 @@ export {};
 fetch("https://jsonplaceholder.typicode.com/posts", { method: "GET" })
   .then((res) => res.json())
   .then(
-    (res: { id: number; userId: number; title: string; body: string }[]) => {
-      res.forEach(creatPost);
+    (
+      postList: { id: number; userId: number; title: string; body: string }[]
+    ) => {
+      const searchInput = document.querySelector("#searchInput");
+      const postContainer = document.getElementById("postContainer");
+
+      searchInput.addEventListener("keyup", () => {
+        postContainer.innerHTML = "";
+        postList.forEach(creatPost);
+      });
+
+      postList
+        .filter((post, textToSearch: any): any => {
+          const keys = Object.keys(post);
+          const result = keys.find((key) => {
+            const value = post[key].toString().toLowerCase();
+            return value.includes(textToSearch.toLowerCase());
+          });
+          return !!result;
+        })
+        .forEach(creatPost);
     }
   );
 
 function creatPost(post) {
-  const htmlPost = `
+  const htmlPost = `s
 <div class="card mb-4" id="post by ${post.userId} ">
   <div class="card-header">
     <h5 class="card-title">${post.id} - ${post.title}</h5>
@@ -27,7 +46,4 @@ function creatPost(post) {
   </div>
 </div>
 `;
-  const newDiv = document.createElement("div");
-  newDiv.innerHTML = htmlPost;
-  document.getElementById("postContainer").appendChild(newDiv);
 }
