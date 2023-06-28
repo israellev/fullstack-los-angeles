f9d74250156148de8d62e20cebf8cbab13f6f417
Brahanu Bayena <brahanu@trlst.local>
2023-06-28 21:29:01 +0300

    some message

--- nodejs/01 - 28.06/class-puzzle/Avraham/server.js
+++ nodejs/01 - 28.06/class-puzzle/Avraham/server.js
@@ -1,14 +1,11 @@
-const express = requ('express');
+const http = require('http');
 
-// Create the Express app
-const app = express();
-
-// Define a route
-app.get('/', (req, res) => {
-  res.send('Hello Avraham');
+// Create the server
+const server = http.createServer((req, res) => {
+  res.end('Hello Avraham');
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+server.listen(3000, () => {
+  console.log(`Server listening on port ${3000}`);
 });
