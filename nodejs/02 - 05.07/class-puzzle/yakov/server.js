870125bf65fa84f0dd6c8989ce4ab8b63ee066fa
yakovmangisto <yakov20286@gmail.com>
2023-07-05 19:25:25 +0300

    some message

--- nodejs/02 - 05.07/class-puzzle/yakov/server.js
+++ nodejs/02 - 05.07/class-puzzle/yakov/server.js
@@ -1,44 +1,18 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+
+// Create an Express application
 const app = express();
-app.use(bodyParser.json());
-
 const posts = [];
-let id = 1;
-
+// Define a route handler for the root path
 app.get("/", (req, res) => {
-  res.send("hello world!");
+  res.send("Hello World??");
 });
 
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.get("/posts/:id", (req, res) => {
-  const postId = +req.params.id;
-  const post = posts.find((post) => post.id === postId);
-  res.send(post);
-});
-
-app.post("/posts", (req, res) => {
-  console.log(req.body);
-
-  const newPost = {
-    ...req.body,
-    id,
-  };
-  id++;
-  posts.push(newPost);
-  res.send(newPost);
-});
-
-app.delete("/posts/:id", (req, res) => {
-  const postId = +req.params.id;
-  const postIndex = posts.findIndex((post) => post.id === postId);
-  res.send(posts[postIndex]);
-  posts.splice(postIndex, 1);
-});
-
+// Start the server
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server listening on port http://localhost:${port}`);
