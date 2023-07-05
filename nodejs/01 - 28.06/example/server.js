1edacb12ea286c01eb3037a592e12352111cd1ed
שרה דובה <sahalo57749@gmail.com>
2023-07-05 14:40:59 +0300

    some message

--- nodejs/01 - 28.06/example/server.js
+++ nodejs/01 - 28.06/example/server.js
@@ -1,13 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 // Create an Express application
 const app = express();
 
 // Define a route handler for the root path
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
+app.get("/api", (req, res) => {
+  res.send({ person: "zematch", age: "32" });
+});
 // Start the server
 const port = 3000;
 app.listen(port, () => {
