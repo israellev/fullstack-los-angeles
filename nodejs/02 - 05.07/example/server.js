39ebfe52e93ad87343c329231dccfcc5574c7569
isreallev <israellev@water-io.com>
2023-07-05 21:07:38 +0300

    some message

--- nodejs/02 - 05.07/example/server.js
+++ nodejs/02 - 05.07/example/server.js
@@ -1,13 +1,22 @@
+// imports
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// create app
 const app = express();
+
+// middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = []
 let id = 1;
 
-app.get('/', (req, res) => {
-  res.send('hello world!');
+
+// routes
+app.get('/hello-world', (req, res) => {
+  res.send("hello world");
 });
 
 app.get('/posts', (req, res) => {
@@ -22,7 +31,6 @@ app.get('/posts/:id', (req, res) => {
 
 app.post('/posts', (req, res) => {
   console.log(req.body)
-  
   const newPost = {
     ...req.body,
     id,
@@ -39,6 +47,10 @@ app.delete('/posts/:id', (req, res) => {
   posts.splice(postIndex, 1);
 })
 
+// middleware
+app.use(express.static('front'));
+
+// deploy
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server listening on port http://localhost:${port}`);
