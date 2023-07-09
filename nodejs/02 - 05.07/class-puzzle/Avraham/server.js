41ef134a8613283875fbe115ec1c0748b56bbac3
Brahanu Bayena <brahanu@trlst.local>
2023-07-09 21:32:24 +0300

    some message

---
+++ nodejs/02 - 05.07/class-puzzle/Avraham/server.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+
+const posts = [];
+let id = 1;
+
+app.get('/', (req, res) => {
+  res.send(posts);
+});
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+  console.log(req.body)
+  const newPost ={
+    ...req.body,
+    id,
+  }
+  id++
+
+  posts.push(newPost)
+  
+  res.send(posts);
+});
+
+app.delete('/posts/:id',(req,res)=>{
+  const postId = +req.params.id;
+  const postIndex = posts.findIndex(post => post.id === postId)
+  posts.splice(postIndex,1)
+  res.send(posts[postIndex])
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port http://localhost:${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get(`/posts`,(req,res)=>{
+//   res.send(posts);
+// })
+
+// app.get('/posts/:id', (req, res) => {
+//   const postId = +req.params.id;
+//   const post = posts.find(post => post.id === postId)
+//   res.send(post);
+// });
+
+// app.post('/posts', (req, res) => {
+//   console.log(req.body)
+//   res.send(posts)
+ 
+// });
