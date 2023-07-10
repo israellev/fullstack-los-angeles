e98d8af935f6d54102f80bc6785765550bced7b7
daniel <danielperetz620@gmail.com>
2023-07-10 22:16:07 +0300

    some messege

--- nodejs/03 - 10.07/class-puzzle/daniel peretz/server.js
+++ nodejs/03 - 10.07/class-puzzle/daniel peretz/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const postsRouter = require('./router/posts')
 
 // create an Express application
 const app = express();
@@ -9,42 +10,17 @@ const app = express();
 // middleware
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static('front'));
 
-const posts = []
-let id = 1;
+// router
+app.use('/posts', postsRouter);
 
-// Define a route handler for the root path
+// routes
 app.get('/hello-world', (req, res) => {
-  res.send('Hello, World!');
+  res.send("hello world");
 });
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-})
 
-app.get('/posts/:id', (req, res) => {
-  const postId = +req.params.id;
-  const post = posts.find(post => post.id === postId)
-  res.send(post);
-});
-
-app.post('/posts', (req, res) => {
-  console.log(req.body)
-  const newPost = {
-    ...req.body,
-    id,
-  }
-  id++
-  posts.push(newPost)
-  res.send(newPost)
-})
-
-app.delete('/posts/:id', (req, res) => {
-  const postId = +req.params.id;
-  const postIndex = posts.findIndex(post => post.id === postId);
-  res.send(posts[postIndex])
-  posts.splice(postIndex, 1);
-})
 
 // middleware
 app.use(express.static('front'));
