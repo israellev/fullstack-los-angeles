544b456a499c22e4d0fdff5dc3a2cd48e3b97134
שרה דובה <sahalo57749@gmail.com>
2023-07-12 21:12:29 +0300

    a

--- nodejs/02 - 05.07/class-puzzle/yzhak/server.js
+++ nodejs/02 - 05.07/class-puzzle/yzhak/server.js
@@ -1,18 +1,55 @@
+// imports
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-// Create an Express application
+// create app
 const app = express();
+
+// middleware
+app.use(bodyParser.json());
+app.use(cors());
+
 const posts = [];
-// Define a route handler for the root path
-app.get("/", (req, res) => {
-  res.send("Hello World??");
+let id = 1;
+
+// routes
+app.get("/hello-world", (req, res) => {
+  res.send("hello world");
 });
 
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-// Start the server
+app.post("/posts", (req, res) => {
+  console.log(req.body);
+  const newPost = {
+    ...req.body,
+    id,
+  };
+  id++;
+  posts.push(newPost);
+  res.send(newPost);
+});
+
+app.get("/posts/:id", (req, res) => {
+  const postId = +req.params.id;
+  const post = posts.find((post) => post.id === postId);
+  res.send(post);
+});
+
+app.delete("/posts/:id", (req, res) => {
+  const postId = +req.params.id;
+  const postIndex = posts.findIndex((post) => post.id === postId);
+  res.send(posts[postIndex]);
+  posts.splice(postIndex, 1);
+});
+
+// middleware
+app.use(express.static("front"));
+
+// deploy
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server listening on port http://localhost:${port}`);
