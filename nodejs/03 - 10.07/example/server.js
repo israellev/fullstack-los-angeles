11472b296d60ee69472998629912ff2000fecbbe
sapir peretz <sapsa1384@gmail.com>
2023-07-12 17:08:26 +0300

    sapir home puzzel

--- nodejs/03 - 10.07/example/server.js
+++ nodejs/03 - 10.07/example/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const postsRouter = require('./router/posts');
+const postsRouter = require('./router/posts')
 
 // create app
 const app = express();
