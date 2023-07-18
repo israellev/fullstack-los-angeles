380d4240f199aba2ce5aa9a43e6bb4b78d874d7c
Mordechai <yonatan.mordechai@hp.com>
2023-07-18 23:34:13 +0300

    class

--- nodejs/02 - 05.07/class-puzzle/YonatanM/server.js
+++ nodejs/02 - 05.07/class-puzzle/YonatanM/server.js
@@ -2,13 +2,10 @@ const http = require('http');
 
 // Create the server
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain')
-
   res.end('Hello World!!!');
 });
 
-const port = 3001;
 // Start the server
-server.listen(port, () => {
-  console.log(`Server listening on port ${3001}`);
+server.listen(3000, () => {
+  console.log(`Server listening on port ${3000}`);
 });
