6bbd17d83db755278ca6f8e8e36a1354c0282d13
Mordechai <yonatan.mordechai@hp.com>
2023-08-01 02:06:59 +0300

    class

--- react/02-31.07/class-puzzle/YonatanM/todo-list-app/src/App.tsx
+++ react/02-31.07/class-puzzle/YonatanM/todo-list-app/src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
 
 function App() {
   return (
@@ -22,5 +26,17 @@ function App() {
     </div>
   );
 }
+
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={TodoList} />
+        <Route path="/add" Component={AddTodo} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ react/02-31.07/class-puzzle/YonatanM/todo-list-app/src/components/AddTodo.tsx
@@ -0,0 +1,9 @@
+
+import React from 'react';
+
+const AddTodo: React.FC = () => {
+  // Your AddTodo component code here
+  return <div>Add Todo</div>;
+};
+
+export default AddTodo;
