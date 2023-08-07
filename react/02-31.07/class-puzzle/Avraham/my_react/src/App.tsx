03ad6ca46870c4529e4058f4b1302526d04afc98
asta054 <avibbb12@gmail.com>
2023-08-07 21:09:31 +0300

    some message

--- react/02-31.07/class-puzzle/Avraham/my_react/src/App.tsx
+++ react/02-31.07/class-puzzle/Avraham/my_react/src/App.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, BrowserRouter, Link } from "react-router-dom";
+import { AppBar, CssBaseline, Tab, Tabs } from "@mui/material";
+import { AddTodo } from './component/AddTodo';
+import { tab } from '@testing-library/user-event/dist/tab';
+import { TodoList } from './component/TodoList';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="">
+    <BrowserRouter>
+      <CssBaseline />
+      <AppBar position="static" style={{ background: "#2196F3" }}>
+        <Tabs
+          centered
+          value={tab}
+          
+          indicatorColor="secondary"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Tab label="Todo List" style={{ color: "white" }} component={Link} to="/" />
+          <Tab label="Add Todo" style={{ color: "white" }} component={Link} to="/add"/>
+        </Tabs>
+      </AppBar>
+      <Routes>
+        <Route path="/" Component={() => <TodoList/>} />
+        <Route path="/add" Component={() => <AddTodo/>} />
+      </Routes>
+    </BrowserRouter>
+      </div>
   );
 }
 
