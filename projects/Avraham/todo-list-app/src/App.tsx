a3dc54320e0b17c7645b3d9c1c28f267ea002844
asta054 <avibbb12@gmail.com>
2023-08-09 20:56:19 +0300

    some message

---
+++ projects/Avraham/todo-list-app/src/App.tsx
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from "react";
+import "./App.css";
+import { AppBar, CssBaseline, Tab, Tabs } from "@mui/material";
+
+
+
+
+function App() {
+
+  return (
+    <>
+      <CssBaseline />
+    </>
+  );
+}
+
+export default App;
