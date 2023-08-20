7185cf76e0cd8ff2b6ffd8540a10a02079ea1c9b
daniel <danielperetz620@gmail.com>
2023-08-20 19:10:02 +0300

    s messege

--- projects/daniel peretz/Cv-generator/src/components/CvForm.tsx
+++ projects/daniel peretz/Cv-generator/src/components/CvForm.tsx
@@ -1,16 +1,7 @@
-import {
-  initialExperience,
-  useGlobalCv,
-} from "../global-hooks/useGlobalCv";
-import {
-  TextField,
-  Button,
-  Typography,
-  Grid,
-  Paper,
-  Box,
-} from "@mui/material";
-import { Experience } from "./Experience";
+import { useGlobalCv } from "../global-hooks/useGlobalCv";
+import { TextField, Button, Typography, Grid, Box, Card } from "@mui/material";
+import { ExperienceForm } from "./ExperienceForm";
+import { SkillsForm } from "./SkillsForm";
 
 interface Props {
   className?: string;
@@ -20,30 +11,39 @@ export function CvForm(props: Props) {
   const { className } = props;
   const { cvData, setCvData } = useGlobalCv();
 
-  const addExperience = () => {
-    const updatedCv = { ...cvData };
-    updatedCv.experience.push({ ...initialExperience });
-    setCvData(updatedCv);
-  };
-
   return (
-    <Box maxWidth="md" className={className}>
-      <Paper elevation={3} sx={{ padding: 3 }}>
-        <Typography variant="h4" gutterBottom>
-          CV Form
-        </Typography>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6}>
+    <Box className={className}>
+      <Card
+        elevation={3}
+        className="hide-scroll"
+        style={{ maxWidth: "800px", margin: "auto"}}
+        sx={{ overflow: "auto", pt: 3, height: "100vh", boxShadow: '0 -2px 55px -13px rgba(0,0,0,.25)' }}
+      >
+        <Grid
+          container
+          spacing={3}
+          p={2}
+        >
+          {/* <Grid item xs={12} sm={12}>
+            <Typography variant="h4" gutterBottom>
+              CV Form
+            </Typography>
+          </Grid> */}
+
+          <Grid item>
             <TextField
+              size="small"
               label="Name"
+              placeholder="enter your name... "
               fullWidth
               value={cvData.name}
               onChange={(e) => setCvData({ ...cvData, name: e.target.value })}
             />
           </Grid>
 
-          <Grid item xs={12} sm={6}>
+          <Grid item>
             <TextField
+              size="small"
               label="Title"
               fullWidth
               value={cvData.title}
@@ -51,8 +51,9 @@ export function CvForm(props: Props) {
             />
           </Grid>
 
-          <Grid item xs={12} sm={6}>
+          <Grid item>
             <TextField
+              size="small"
               label="Email"
               fullWidth
               value={cvData.email}
@@ -60,8 +61,9 @@ export function CvForm(props: Props) {
             />
           </Grid>
 
-          <Grid item xs={12} sm={6}>
+          <Grid item>
             <TextField
+              size="small"
               label="LinkedIn"
               fullWidth
               value={cvData.linkedin}
@@ -71,29 +73,33 @@ export function CvForm(props: Props) {
             />
           </Grid>
 
-          <Grid item xs={12} sm={6}>
+          <Grid item >
             <TextField
+              size="small"
               label="Phone"
               fullWidth
               value={cvData.phone}
               onChange={(e) => setCvData({ ...cvData, phone: e.target.value })}
             />
           </Grid>
-
-          {/* Add more fields here for other details like title, email, linkedin, phone, etc. */}
-
-          <Grid item xs={12}>
-            <Typography variant="h6">Experience</Typography>
-            {cvData.experience.map((experience, index) => (
-              <Experience key={index} index={index} experience={experience} />
-            ))}
-
-            <Grid item xs={12}>
-              <Button variant="outlined" onClick={addExperience}>
-                Add Experience
-              </Button>
-            </Grid>
+          <Grid item>
+            <TextField
+              size="small"
+              label="Summary"
+              type="text"
+              multiline
+              rows={5}
+              fullWidth
+              value={cvData.summary}
+              onChange={(e) =>
+                setCvData({ ...cvData, summary: e.target.value })
+              }
+            />
           </Grid>
+
+          <ExperienceForm />
+
+          <SkillsForm />
 
           <Grid item xs={12}>
             <Button variant="contained" color="primary">
@@ -101,7 +107,7 @@ export function CvForm(props: Props) {
             </Button>
           </Grid>
         </Grid>
-      </Paper>
+      </Card>
     </Box>
   );
 }
