2fff69b345bf47b395fd3d9f3e14415864bc6f8e
israellev <israellev770@gmail.com>
2023-08-28 19:26:53 +0300

    add more tamplates, choose template

--- projects/israel/cv-generator/src/components/CvResult/CvResult.tsx
+++ projects/israel/cv-generator/src/components/CvResult/CvResult.tsx
@@ -1,24 +1,21 @@
 import { useGlobalCv } from "../../global-hooks/useGlobalCv";
 import { Box } from "@mui/material";
 import { templateList } from "../CvDesign/SelectTemplate/SelectTemplate";
-import { FloatRuler } from "./FloatRuler";
 
 interface Props {
   templateIndex: number;
 }
 
 export const CvResult = (props: Props) => {
-  const {templateIndex} = props;
+  const { templateIndex } = props;
   const { cvData } = useGlobalCv();
 
-  const TemplateComponent = templateList[templateIndex].component
+  const TemplateComponent = templateList[templateIndex].component;
   return (
-    <Box sx={{backgroundColor: "#f5f5f5"}}>
-      <Box sx={{height: "88vh"}}>
-      <TemplateComponent cvData={cvData}/>
-
+    <Box sx={{ backgroundColor: "#f5f5f5" }}>
+      <Box sx={{ height: "95vh", position: 'relative' }}>
+        <TemplateComponent cvData={cvData} />
       </Box>
-      <FloatRuler/>
     </Box>
   );
 };
--- projects/israel/cv-generator/src/project.md
+++ projects/israel/cv-generator/src/project.md
@@ -8,7 +8,7 @@ V 3. Edit on the left, show cv result on the middle
 V 4. Add 2 tamplates on the right
 V 5. Add color picker
 V 6. design touch ups
-7. Save as file
+V 7. Save as file
 
 
 Continue tasks:
