6fe9da77699c0919f886094b905ac00bd0bd8e01
israellev <israellev770@gmail.com>
2023-12-18 19:44:18 +0200

    add more tamplates, choose template

---
+++ projects/israel/store/front/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Products } from './components/Products';
+
+function App() {
+  return (
+    <div>
+      <Products/>
+    </div>
+  );
+}
+
+export default App;
