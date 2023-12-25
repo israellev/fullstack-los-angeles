89a21c3b1fbdbf8a50418225c5af15a09a4db22d
israellev <israellev770@gmail.com>
2023-12-25 19:23:09 +0200

    m

--- projects/israel/store/server.js
+++ projects/israel/store/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv')
 const path = require('path');
 const app = express();
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 
+// load variables from ".env" file
+dotenv.config()
+
+// connect to database
 const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const database = client.db("test");
 try {
@@ -13,9 +17,11 @@ try {
     console.error(e);
 }
 
+// allow interact with the server from other sites
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'front', 'build')));
 
+// serve all react static files 
+app.use(express.static(path.join(__dirname, 'front', 'build')));
 
 app.get('/hello-world', (req, res) => {
     res.send();
